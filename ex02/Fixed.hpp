37e70f36083b7ba7eb0dca87846e20e72fcd165f
SpaceChae <enaenen@naver.com>
2022-08-19 23:43:34 +0900

    pre

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed {
+ private:
+  int fixedPointNum;
+  static const int fractionalBits = 8;
+
+ public:
+  Fixed(void);
+  Fixed(const int fpn);
+  Fixed(const float fpn);
+  Fixed(const Fixed& fixed);
+  ~Fixed();
+
+  Fixed& operator=(const Fixed& op);
+  Fixed operator+(const Fixed& op);
+  Fixed operator-(const Fixed& op);
+  Fixed operator*(const Fixed& op);
+  Fixed operator/(const Fixed& op);
+
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+
+  float toFloat(void) const;
+  int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
